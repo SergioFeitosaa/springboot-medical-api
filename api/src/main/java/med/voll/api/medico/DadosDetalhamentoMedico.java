8936efde4412a76b0352629473aaaed10981bfd9
Sergio Ricardo Feitosa <sergio.feitosa2@gmail.com>
2026-03-18 22:15:06 -0300

    feat: implementa boas práticas REST com retorno adequado de HTTP status (200, 404, 201)

---
+++ api/src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import med.voll.api.infrastructure.entity.endereco.Endereco;
+import med.voll.api.infrastructure.entity.medico.Medico;
+
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+
+    public DadosDetalhamentoMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(),medico.getEndereco());
+    }
+}
